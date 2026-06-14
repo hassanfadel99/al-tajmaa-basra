839fc349ae6e51233a66dd7cd64a42e3a18d6bf9
hassanfadel99 <sabahfadel9@gmail.com>
2026-06-14 21:55:38 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,8 @@
 self.addEventListener('install', (e) => {
   console.log('[Service Worker] Install');
 });
+
+// هذا السطر هو السر! بدونه لن يظهر إشعار التثبيت التلقائي
+self.addEventListener('fetch', (e) => {
+    // المتصفح يشترط وجود هذه الدالة ليعتبر الموقع تطبيقاً قابلاً للتحميل (PWA)
+});
